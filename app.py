6892532a4f41ab52142788d0a9327910ce538dba
Kalavalapudi Naga Shanushka <nagashanushka@gmail.com>
2026-03-20 12:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request 
 import openai
 app=Flask(__name__)
-openai.api_key="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"
+# openai.api_key="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"
 def generate_message(name, company):
     prompt = f"""
     Write a professional sales outreach message to {name} from {company}.
     Keep it short, friendly, and persuasive.
     """
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini",
-        messages=[{"role": "user", "content": prompt}]
-    )
+   from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": prompt}]
+)
+
+return response.choices[0].message.content
     return response['choices'][0]['message']['content']
 def lead_score(company):
     score = len(company) * 10
